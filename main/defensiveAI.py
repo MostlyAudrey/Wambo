b3f60c4bf90a931117eec4da1ac73c84bab6d97f
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-03-24 11:12:52 -0400

    defensive: slow down AI movement

--- main/defensiveAI.py
+++ main/defensiveAI.py
@@ -2,6 +2,10 @@ from playerIntelligence import PlayerIntelligence
 
 
 class DefensiveAI(PlayerIntelligence):
+    # This counter is used to slow the AI down, so we can see how it is making its move
+    # TODO - I'm not sure if this is the best way to do this, so if there is one, please fix this
+    wait = 0
+    threshold = 100
 
     def picking_piece(self, mouse_click):
         # TODO actually make a decision on what piece to pick
@@ -12,11 +16,18 @@ class DefensiveAI(PlayerIntelligence):
         return 1
 
     def show_possible_moves(self, mouse_click):
+        self.wait += 1
+
         if self.possible_moves is None:
             self.possible_moves = PlayerIntelligence.compute_possible_moves(self, self.selected_piece)
             PlayerIntelligence.draw_possible_moves(self, self.possible_moves, self.selected_piece)
             return 0
         else:
+            if self.wait < self.threshold:
+                return 0
+            else:
+                self.wait = 0
+
             node = self.possible_moves[0]
             self.selected_piece.undrawBorder()
             node.remove_highlight()
